6ba07bc921197e21213ce061fae9f1f886a3e4d4
Jeferson Menegazzo <jef.menegazzo@outlook.com>
2020-02-04 16:50:47 -0300

    Minor fixes.

--- raw_data_processing/1 - Raw Data Fix Correction.py
+++ raw_data_processing/1 - Raw Data Fix Correction.py
@@ -7,64 +7,64 @@ workspace = {
         "saveiro-1": {
             "readFolder": "2019-12-24 17-21-12 - Saveiro Cenário 1",
             "saveFolder": "PVS 1",
-            "right_shift": 2,
-            "left_shift": 2,
+            "right_shift": 2.08,
+            "left_shift": 2.08,
             "readme": "Car: Saveiro"
         },
         "saveiro-2": {
             "readFolder": "2019-12-24 17-59-52 - Saveiro Cenário 2",
             "saveFolder": "PVS 2",
-            "right_shift": 2,
-            "left_shift": 2,
+            "right_shift": 1.77,
+            "left_shift": 1.77,
             "readme": "Car: Saveiro"
         },
         "saveiro-3": {
             "readFolder": "2019-12-24 18-37-13 - Saveiro Cenário 3",
             "saveFolder": "PVS 3",
-            "right_shift": 2,
-            "left_shift": 2,
+            "right_shift": 1.87,
+            "left_shift": 1.87,
             "readme": "Car: Saveiro"
         },
         "bravo-1": {
             "readFolder": "2019-12-25 17-44-48 - Bravo Cenário 1",
             "saveFolder": "PVS 4",
-            "right_shift": 2,
-            "left_shift": 2,
+            "right_shift": 0.6,
+            "left_shift": 0.6,
             "readme": "Car: Bravo"
         },
         "bravo-2": {
             "readFolder": "2019-12-25 18-17-03 - Bravo Cenário 2",
             "saveFolder": "PVS 5",
-            "right_shift": 2,
-            "left_shift": 2,
+            "right_shift": 0.6,
+            "left_shift": 0.6,
             "readme": "Car: Bravo"
         },
         "bravo-3": {
             "readFolder": "2019-12-25 18-44-19 - Bravo Cenário 3",
             "saveFolder": "PVS 6",
-            "right_shift": 2,
-            "left_shift": 2,
+            "right_shift": 0.9,
+            "left_shift": 0.9,
             "readme": "Car: Bravo"
         },
         "palio-1": {
             "readFolder": "2019-12-26 18-24-09 - Palio Cenário 1",
             "saveFolder": "PVS 7",
-            "right_shift": 2,
-            "left_shift": 2,
+            "right_shift": 0,
+            "left_shift": 0,
             "readme": "Car: Palio"
         },
         "palio-2": {
             "readFolder": "2019-12-26 18-51-03 - Palio Cenário 2",
             "saveFolder": "PVS 8",
-            "right_shift": 2,
-            "left_shift": 2,
+            "right_shift": 0,
+            "left_shift": 0,
             "readme": "Car: Palio"
         },
         "palio-3": {
             "readFolder": "2019-12-26 19-23-31 - Palio Cenário 3",
             "saveFolder": "PVS 9",
-            "right_shift": 2,
-            "left_shift": 2,
+            "right_shift": 0,
+            "left_shift": 0,
             "readme": "Car: Palio"
         }
     }
